b6336ed653a3a2af5837e4558434077bd7649161
junhyung park <87253932+junhyungdev@users.noreply.github.com>
2024-04-25 16:00:19 +0900

    initial commit

---
+++ src/test/java/probono/gcc/school/SchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package probono.gcc.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
