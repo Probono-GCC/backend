bcfa16a75d64a695b59d737c34e5a892d9a0f2b4
JunHyung <xk130@naver.com>
2024-08-05 19:01:58 +0900

    Update AppConfig file

--- src/main/java/probono/gcc/school/config/AppConfig.java
+++ src/main/java/probono/gcc/school/config/AppConfig.java
@@ -1,10 +1,6 @@
 package probono.gcc.school.config;
 
-import org.modelmapper.ModelMapper;
-import org.modelmapper.PropertyMap;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import probono.gcc.school.model.dto.TeacherCreateRequestDto;
 
 @Configuration
 public class AppConfig {
