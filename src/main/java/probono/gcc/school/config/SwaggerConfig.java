eb8fddd29488beca5083a80e081f5412927d0b0c
137pjy <pjy137@konkuk.ac.kr>
2024-08-17 21:00:57 +0900

    Delete swagger implementation to match version of springboot

--- src/main/java/probono/gcc/school/config/SwaggerConfig.java
+++ src/main/java/probono/gcc/school/config/SwaggerConfig.java
@@ -11,14 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.SecurityReference;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
-import springfox.documentation.spring.web.plugins.Docket;
+
 
 @Configuration
 @RequiredArgsConstructor
